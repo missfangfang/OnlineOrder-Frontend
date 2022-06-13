3cbcf21655f0869862a6413352431cde05fcabc9
missfangfang <tiffanyfang67@gmail.com>
2022-06-13 13:39:29 -0700

    Ant Design layout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { Layout } from "antd";
 
 const { Header, Content } = Layout;
 
+// App是一个component，因为在index.js里面有<App />
 function App() {
   return (
+    // 100vh (viewport height) - 100%占满整个网页页面
+    // Ant Design固定header高度64px
     <Layout style={{ height: "100vh" }}>
-      <Header>header</Header>
+      <Header>header</Header>  
       <Content
         style={{
           padding: "50px",
-          maxHeight: "calc(100% - 64px)",
-          overflowY: "auto",
+          maxHeight: "calc(100% - 64px)", // 这是一个css函数：content元素的爹的100%减去64px
+          overflowY: "auto",  // Y轴溢出前不做任何处理，auto在溢出的时候会产生一个scroll bar
         }}
       >
         content
